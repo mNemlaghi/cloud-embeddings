94f23aa8951439a16073548d7e6f59e185e7ec49
mNemlaghi <mehdi.nemlaghi@gmail.com>
2023-09-05 12:57:54 +0000

    update finetune

--- finetune/peftscripts/train.py
+++ finetune/peftscripts/train.py
@@ -1,8 +1,11 @@
 
 
+
+
 import torch
 from torch import nn
 from transformers import AutoTokenizer, AutoModel
+from transformers import TrainingArguments, Trainer, logging
 from tqdm import tqdm
 import argparse
 
@@ -14,87 +17,88 @@ from datasets import load_dataset, Dataset
 from torch.utils.data import DataLoader
 from transformers import default_data_collator
 import os
+#import evaluate
 
 
-MAX_LENGTH=70
+#roc_auc_score = evaluate.load("roc_auc")
+
+MAX_LENGTH=240
 
 
 class EncoderForESCI(nn.Module):
     def __init__(self, pretrained_model, lora = True, normalize=True, lora_rank=8):
         super(EncoderForESCI, self).__init__()
         self.tokenizer = AutoTokenizer.from_pretrained(pretrained_model)
-        self.model = AutoModel.from_pretrained(pretrained_model)
+        self.embedding_model = AutoModel.from_pretrained(pretrained_model)
         if lora:
             config = LoraConfig(
                 r=lora_rank,
-                lora_alpha=32,
-                #target_modules=["key","query", "value"],
+                lora_alpha=16,
+                target_modules=["key","query", "value"],
                 #target_modules=["encoder.layer.*"],
-                target_modules=None,
                 bias="none",
                 lora_dropout=0.05,
                 inference_mode=False,
                 task_type=TaskType.FEATURE_EXTRACTION
             )
         
-            self.model = get_peft_model(self.model, config)
+            self.embedding_model = get_peft_model(self.embedding_model, config)
         self.normalize=normalize
         
-    def forward(self, **kwargs):
-        model_output = self.model(**kwargs)
+    def forward(self, query_input_ids, query_attention_mask, product_input_ids, product_attention_mask,
+                query_token_type_ids, product_token_type_ids, labels):
+        q = {"input_ids":query_input_ids.squeeze(1), "attention_mask":query_attention_mask.squeeze(1),
+            "token_type_ids":query_token_type_ids.squeeze(1)}
+        p = {"input_ids":product_input_ids.squeeze(1), "attention_mask":product_attention_mask.squeeze(1),
+            "token_type_ids":product_token_type_ids.squeeze(1)}
         
-        embeddings = self.mean_pooling(model_output, kwargs["attention_mask"])
+        q_emb_all = self.embedding_model(**q)
+        p_emb_all = self.embedding_model(**p)
+        
+        q_emb = self.mean_pooling(q_emb_all, q["attention_mask"])
+        p_emb = self.mean_pooling(p_emb_all, p["attention_mask"])
+        #q_emb, p_emb = self.mean_pooling()), self.model(**p)
+        
+        
         if self.normalize:
-            embeddings = torch.nn.functional.normalize(embeddings, p=2, dim=1)
-        return embeddings
-      
+            q_emb = torch.nn.functional.normalize(q_emb, p=2, dim=1)
+            p_emb = torch.nn.functional.normalize(p_emb, p=2, dim=1)
+        
+        contig_labels=labels.reshape(-1).contiguous()
+        return q_emb, p_emb, contig_labels
+        
     def mean_pooling(self, model_output, attention_mask):
         token_embeddings = model_output[0]  # First element of model_output contains all token embeddings
         input_mask_expanded = attention_mask.unsqueeze(-1).expand(token_embeddings.size()).float()
         return torch.sum(token_embeddings * input_mask_expanded, 1) / torch.clamp(input_mask_expanded.sum(1), min=1e-9)
 
-
-def print_trainable_parameters(model):
-    """
-    Prints the number of trainable parameters in the model.
-    """
-    trainable_params = 0
-    all_param = 0
-    for _, param in model.named_parameters():
-        all_param += param.numel()
-        if param.requires_grad:
-            trainable_params += param.numel()
-    print(
-        f"trainable: {trainable_params} || all: {all_param} || trainable%: {100 * trainable_params / all_param}"
-    )
-
+    
 def tokenize_examples_and_target(examples):
     queries = examples["query"]
     result = finetuned.tokenizer(queries, padding="max_length", max_length=MAX_LENGTH, truncation=True, return_tensors='pt')
-    result = {f"query_{k}": v.reshape(-1) for k, v in result.items()}
+    #result = finetuned.tokenizer(queries, return_tensors='pt')
+    result = {f"query_{k}": v for k, v in result.items()}
 
     products = examples["product_title"]
     result_products = finetuned.tokenizer(products, padding="max_length", max_length=MAX_LENGTH, truncation=True, return_tensors='pt')
+    #result_products = finetuned.tokenizer(products, return_tensors = 'pt')
     for k, v in result_products.items():
-        result[f"product_{k}"] = v.reshape(-1)
-
-    result["labels"] = torch.ByteTensor([examples["relevance_label"]]).reshape(-1)
+        result[f"product_{k}"] = v
+        
+    result["labels"] = torch.ByteTensor([examples["relevance_label"]])
     return result
 
 
-def full_forward_pass(batch, finetuned):
-    q = {k.replace("query_", ""):v.to('cuda') for k,v in batch.items() if 'query_' in k} 
-    p = {k.replace("product_", ""):v.to('cuda') for k,v in batch.items() if 'product_' in k} 
-
-    q_emb, p_emb = finetuned(**q), finetuned(**p)
-    labels=batch['labels'].reshape(-1).to('cuda')
-    return q_emb, p_emb, labels
 
 emb_loss=nn.CosineEmbeddingLoss(reduction="mean")
-def get_loss(q_e, p_e, labels):
-    #In cosine loss, targets are 1 or -1, so we transform our 0/1 labels
-    return emb_loss(q_e, p_e, 2*labels - 1)
-    #return torch.square(emb_loss(q_e, p_e, 2*labels - 1)) 
+
+class ESCITrainer(Trainer):  
+    def compute_loss(self, model, inputs, return_outputs = False):
+        outputs = model(**inputs)
+        q_emb, p_emb, labels = outputs
+        custom_loss =  emb_loss(q_emb, p_emb, 2*labels - 1)
+        return (custom_loss, outputs) if return_outputs else custom_loss
+
 
 if __name__=='__main__':
     parser = argparse.ArgumentParser()
@@ -109,8 +113,14 @@ if __name__=='__main__':
     
     parser.add_argument("--model-dir", type=str, default=os.environ["SM_MODEL_DIR"])
     
-    
+        
     args, _ = parser.parse_known_args()
+
+    default_args = {"output_dir": "tmp",
+                    "evaluation_strategy": "steps",
+                    "num_train_epochs": args.epochs,
+                    "log_level": "error",
+                    "report_to": "none"}
 
     
     finetuned = EncoderForESCI(args.pretrained_model, lora_rank=args.lora_rank)
@@ -120,60 +130,21 @@ if __name__=='__main__':
                       num_proc=os.cpu_count(), 
                      remove_columns=ds['train'].column_names)
     
-    train_dataloader = DataLoader(
-        processed_ds['train'],
-        shuffle=True,
-        collate_fn=default_data_collator,
-        batch_size=args.batch_size,
-        pin_memory=True)
-
-    validation_dataloader = DataLoader(
-        processed_ds['validation'],
-        shuffle=True,
-        collate_fn=default_data_collator,
-        batch_size=args.eval_batch_size,
-        pin_memory=True)
     
-    starting_epoch=0
-    finetuned.to('cuda')
-    optimizer = torch.optim.AdamW(finetuned.parameters(), lr=args.lr)
-    for epoch in range(starting_epoch, args.epochs):
-    
-        finetuned.train()
-        running_loss = 0.0
-        for step, batch in enumerate(tqdm(train_dataloader)):            
+    training_args = TrainingArguments(
+        per_device_train_batch_size=args.batch_size,
+        per_device_eval_batch_size=args.eval_batch_size,
+        gradient_accumulation_steps=4,
+        fp16=True,
+        remove_unused_columns=False, 
+    **default_args,
+    )
 
-            q_emb, p_emb, labels = full_forward_pass(batch, finetuned)        
-            loss = get_loss(q_emb, p_emb,labels)
-            
-            loss.backward()
-            optimizer.step()
+    trainer = ESCITrainer(model=finetuned, args=training_args, train_dataset=processed_ds['train'].with_format("torch")
+                      , eval_dataset=processed_ds['validation'].with_format("torch"))
+    trainer.train()
 
-            running_loss += loss.detach().item()
-            if step%100==0:
-                mean_train_loss= running_loss / (step+1)
-                print(f"Epoch {epoch+1}, step {step+1} ==> training loss {mean_train_loss}")
-
-
-        mean_train_loss= running_loss / (step+1)
-        print(f"Epoch {epoch+1}, step {step+1} ==> training loss {mean_train_loss}")
-
-        finetuned.eval()
-        eval_loss=0.0
-        for step, batch in enumerate(tqdm(validation_dataloader)):
-            with torch.no_grad():
-                q_emb, p_emb, labels = full_forward_pass(batch, finetuned)
-                loss = get_loss(q_emb, p_emb,labels)
-            eval_loss+=loss.detach().item()
-            if step%100==0:
-                mean_valid_loss= eval_loss / (step+1)
-                print(f"Epoch {epoch+1}, step {step+1} ==> eval loss {mean_valid_loss}")
-
-
-        mean_valid_loss= eval_loss / (step+1)
-        print(f"Finished Epoch {epoch+1}, step {step+1} ==> eval loss {mean_valid_loss}")
-    
+        
     ## Merge the models with trained Adapters, and save it to /opt/ml/code
-    merged = finetuned.model.merge_and_unload()
+    merged = finetuned.embedding_model.merge_and_unload()
     merged.save_pretrained(args.model_dir)
-    
